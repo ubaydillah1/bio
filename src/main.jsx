305aa202b32b855d8dd1254745a3ad0fa4ae436e
ubaydillah1 <ubaydillah1737@gmail.com>
2024-10-02 21:22:56 +0700

    install react, tailwind, react-router

---
+++ src/main.jsx
@@ -0,0 +1,18 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomePage from "./pages/homePage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
